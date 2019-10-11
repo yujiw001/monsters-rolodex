411d20b4fa1d7269ea2b333b6e2fc0bcc010cfb2
yujiw001 <wany5d1401@gmail.com>
2019-10-11 11:35:08 -0700

    Updates

---
+++ precache-manifest.e8987679b9d03e834c083b389338aaa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b884bdedf0678f4ff3c5bde4224926df",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a86a48e976b80e1fa690",
+    "url": "/monsters-rolodex/static/css/main.aa595dfe.chunk.css"
+  },
+  {
+    "revision": "b0703eff4582a78f493a",
+    "url": "/monsters-rolodex/static/js/2.5075ebff.chunk.js"
+  },
+  {
+    "revision": "a86a48e976b80e1fa690",
+    "url": "/monsters-rolodex/static/js/main.dd1b29d6.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/monsters-rolodex/static/media/logo.25bf045c.svg"
+  }
+]);
